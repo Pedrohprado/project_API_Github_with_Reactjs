8761438b32c3e7eb55fb9ef4ad1e21d270e81e68
Pedro Henrique Prado <pedroh.prado@outlook.com>
2023-10-11 08:04:40 -0300

    new project with API and ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MainPage from "./pages/MainPage";
+import { Normalize } from "styled-normalize";
+
+export default function App() {
+  return (
+    <>
+      <MainPage />
+      <Normalize />
+    </>
+  );
+}
