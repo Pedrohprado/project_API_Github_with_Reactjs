36eb61a2f66230b30b29ded560436be13379b8b0
Pedro Henrique Prado <pedroh.prado@outlook.com>
2023-10-19 16:17:20 -0300

    alteration, but i need refactory

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,35 +1,53 @@
+/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
 import { MdGroup, MdLocationCity, MdWork, MdLink } from "react-icons/md";
 
 import { Container, Header, Avatar, Login, Name, Inner, Data } from "./style";
 
-export default function Profile() {
+export default function Profile({ user }) {
+  const blogFree = () => {
+    const blog = user.blog; //This trying????
+    return blog.slice(6);
+  };
+
   return (
     <Container>
       <Header>
-        <Avatar src="https://avatars.githubusercontent.com/u/67080026?v=4" />
-        <Login>Pedro H</Login>
-        <Name>Pedro Henrique Prado</Name>
+        <Avatar src={user.avatar_url} />
+        <Login>{user.login}</Login>
+        <Name>{user.name}</Name>
       </Header>
       <Inner>
         <Data>
           <MdGroup size={20} />
-          30 seguidores &middot; 10 seguindo
+          {user.following} seguidores &middot; {user.followers} seguindo
         </Data>
         <Data>
           <MdWork size={20} />
-          Pedertractor
+          {user.company}
         </Data>
         <Data>
           <MdLocationCity size={20} />
-          Pederneiras
+          {user.location}
         </Data>
         <Data>
           <MdLink size={20} />
-          <a href="https://www.notion.so/ad4f30285d2f4098a780a8b333a3e4f4?v=f9ef2d1e9ae441f198ef525eeee61d0d">
-            teste
-          </a>
+          <a href={`\\${user.blog}`}>{blogFree()}</a>
         </Data>
       </Inner>
     </Container>
   );
 }
+
+Profile.PropTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -5,10 +5,20 @@ import Filter from "./Filter";
 import Repositories from "./Repositories";
 
 function RepositoriesPage() {
+  const user = {
+    login: "Pedrohprado",
+    name: "Pedro Henrique Prado",
+    avatar_url: "https://avatars.githubusercontent.com/u/67080026?v=4",
+    followers: 1,
+    following: 1,
+    company: null,
+    blog: "https://portfolio-pedro-react.vercel.app/",
+    location: "Bauru/SP",
+  };
   return (
     <Container>
       <Sidebar>
-        <Profile />
+        <Profile user={user} />
         <Filter />
       </Sidebar>
       <Main>
